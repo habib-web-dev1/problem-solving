87d2cd42fa9452209cc7c001b081fead14e129ef
MD AHSAN HABIB <habibmdahsan08@gmail.com>
2026-07-12 10:13:11 +0600

    Count how many times each character appears in a string solved in Day 8

---
+++ Day 8/Count how many times each character appears in a string.js
@@ -0,0 +1,14 @@
+//str="banana"
+function countCharacter(str) {
+  let frequencyMap = {};
+  for (let i = 0; i < str.length; i++) {
+    if (frequencyMap[str[i]] !== undefined) {
+      frequencyMap[str[i]] = frequencyMap[str[i]] + 1;
+    } else {
+      frequencyMap[str[i]] = 1;
+    }
+  }
+  return frequencyMap;
+}
+let str = "bananan";
+console.log(countCharacter(str));

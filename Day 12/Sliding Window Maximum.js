964a0697bfd71747093df2db5d9dcaf6845328ba
MD AHSAN HABIB <habibmdahsan08@gmail.com>
2026-08-02 22:59:29 +0600

    Sliding Window Maximum solved in Day 12

---
+++ Day 12/Sliding Window Maximum.js
@@ -0,0 +1,33 @@
+function maxSlidingWindow(nums, k) {
+  const result = [];
+  if (nums.length === 0 || k <= 0) {
+    return result;
+  }
+
+  const deque = [];
+  let head = 0;
+  let tail = 0;
+
+  for (let right = 0; right < nums.length; right = right + 1) {
+    while (tail > head && nums[deque[tail - 1]] <= nums[right]) {
+      tail = tail - 1;
+    }
+
+    deque[tail] = right;
+    tail = tail + 1;
+
+    if (deque[head] <= right - k) {
+      head = head + 1;
+    }
+
+    if (right >= k - 1) {
+      let maxIndex = deque[head];
+      result[result.length] = nums[maxIndex];
+    }
+  }
+
+  return result;
+}
+
+console.log(maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7, 8], 3));
+console.log(maxSlidingWindow([1, 2], 1));

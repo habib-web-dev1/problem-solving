01ed79b718011bc0e3edc07a14fbae7cc23bfc00
MD AHSAN HABIB <habibmdahsan08@gmail.com>
2026-08-11 18:41:31 +0600

    Update some code

---
+++ Day 14/Find Minimum in Rotated Sorted Array.js
@@ -0,0 +1,20 @@
+function findMin(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let mid = left + Math.floor((right - left) / 2);
+
+    if (nums[mid] > nums[right]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  return nums[left];
+}
+
+console.log(findMin([3, 4, 5, 1, 2]));
+console.log(findMin([4, 5, 6, 7, 0, 1, 2]));
+console.log(findMin([11, 13, 15, 17]));

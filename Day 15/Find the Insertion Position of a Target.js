56267f5b190d4a2281791a29703bd74a6b3b0bb1
MD AHSAN HABIB <habibmdahsan08@gmail.com>
2026-08-26 23:28:45 +0600

    Find the Insertion Position of a Target solved in Day 15

---
+++ Day 15/Find the Insertion Position of a Target.js
@@ -0,0 +1,25 @@
+function searchInsert(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return left;
+}
+
+console.log(searchInsert([1, 3, 5, 6], 5));
+console.log(searchInsert([1, 3, 5, 6], 2));
+console.log(searchInsert([1, 3, 5, 6], 7));
+console.log(searchInsert([1, 3, 5, 6], 0));

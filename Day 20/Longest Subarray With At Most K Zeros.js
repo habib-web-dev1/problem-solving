34969e5750406ffc3384b52505f945dc58714b85
MD AHSAN HABIB <habibmdahsan08@gmail.com>
2026-09-26 23:16:44 +0600

    Longest Subarray With At Most K Zeros solved in Day 20

---
+++ Day 20/Longest Subarray With At Most K Zeros.js
@@ -0,0 +1,30 @@
+function longestOnes(nums, k) {
+  let left = 0;
+  let zeroCount = 0;
+  let maxLength = 0;
+
+  for (let right = 0; right < nums.length; right = right + 1) {
+    if (nums[right] === 0) {
+      zeroCount = zeroCount + 1;
+    }
+
+    while (zeroCount > k) {
+      if (nums[left] === 0) {
+        zeroCount = zeroCount - 1;
+      }
+      left = left + 1;
+    }
+
+    let currentLength = right - left + 1;
+    if (currentLength > maxLength) {
+      maxLength = currentLength;
+    }
+  }
+
+  return maxLength;
+}
+
+console.log(longestOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2));
+console.log(
+  longestOnes([0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], 3),
+);
